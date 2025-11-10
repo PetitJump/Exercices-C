7f43debb7de113786c7f8bca2a38fd6f6e6bd85c
PetitJump <killianbonnin7@gmail.com>
2025-11-10 16:56:14 +0100

    Modification de la fonction demander_prenom()

--- evaluateur.c
+++ evaluateur.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-void demander_prenom(){
-    char prenom[50];
+char* demander_prenom(){
+    static char prenom[50];
     printf("Quel est ton prénom : ");
     scanf("%s", prenom);
     printf("Bonjour %s \n", prenom);
+    return prenom;
 }
 
 int main(){
     int note;
-    demander_prenom();
-    printf("Veuillez donner votre dernière note : ");
+    char* prenom = demander_prenom();
+    printf("%s veuillez donner votre dernière note : ", prenom);
     scanf("%d", &note);
     if (note <= 9) {
         printf("Note insuffisant \n");
