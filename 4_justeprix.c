b7bce0c3c503c29014c5f4cca26fef4ee1e13b05
PetitJump <killianbonnin7@gmail.com>
2025-11-13 19:57:58 +0100

    Ajout d'un exo 4

--- 4_justeprix.c
+++ 4_justeprix.c
@@ -6,10 +6,12 @@ int main(){
     srand(time(NULL));
     int nombre = (rand() % 100) + 1;
     int devine;
+    int essaie = 1;
     printf("Donne un premier chiffre : ");
     scanf("%d", &devine);
 
     while(nombre != devine){
+        essaie += 1;
         if(nombre > devine){
             printf("Trop petit \n");
         }
@@ -19,6 +21,7 @@ int main(){
         printf("Choisir un nouveau chiffre : ");
         scanf("%d", &devine);
     }
-    printf("Bravo vous avez trouver !");
+    printf("Bravo vous avez trouver ! \n");
+    printf("Vous avez trouver en %d essaies !", essaie);
     return 0;
 }
