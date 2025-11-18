5e0cea233879b78d3d8a2d8029c4ecbcf7a617e7
PetitJump <killianbonnin7@gmail.com>
2025-11-18 11:35:30 +0100

    Fin de l'exercice de moyenne

--- 5_moyenne.c
+++ 5_moyenne.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+int somme(int liste[], int taille){
+    int total = 0;
+    for (int i = 0; i < taille; i += 1) {
+        total += liste[i];
+    }
+    return total;
+}
+
 int main(){
-    
+    int moyenne;
+    int notes[] = {20, 13, 10, 0, 2, 20, 15}; // Total des notes
+    int taille = sizeof(notes) / sizeof(notes[0]); // Taille du tableau (souvent sizeof(notes[0]) = 4) 
+    moyenne = somme(notes, taille) / taille;
+    printf("La moyenne : %d", moyenne);
     return 0;
 }
